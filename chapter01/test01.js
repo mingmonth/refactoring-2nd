6c5cf03c4c663b5e5fc5ddd11d0c6d10df3e6435
ys9922 <ys9922@naver.com>
2021-04-16 13:40:21 +0900

    변수명 명확한 이름으로 변경(thisAmount --> result, perf --> performance)

--- chapter01/test01.js
+++ chapter01/test01.js
@@ -3,26 +3,26 @@ const fs = require("fs");
 const plays = JSON.parse(fs.readFileSync("./plays.json"));
 const invoices = JSON.parse(fs.readFileSync("./invocies.json"));
 
-function amoutFor(perf, play) {
-  let thisAmount = 0;
+function amoutFor(aPerformance, play) {
+  let result = 0;
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 function statement(invoice, plays) {
@@ -35,17 +35,18 @@ function statement(invoice, plays) {
     minumumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice[0].performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amoutFor(perf, play);
+  for (let performance of invoice[0].performances) {
+    const play = plays[performance.playID];
+    let result = amoutFor(performance, play);
 
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += Math.max(performance.audience - 30, 0);
+    if ("comedy" === play.type)
+      volumeCredits += Math.floor(performance.audience / 5);
 
-    result += `${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
+    result += `${play.name}: ${format(result / 100)} (${
+      performance.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += result;
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
