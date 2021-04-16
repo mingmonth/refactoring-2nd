5b3df38ecfcf44174efee7fcd9f25f84ee09303a
ys9922 <ys9922@naver.com>
2021-04-16 13:25:09 +0900

    accountFor 함수 추출

--- chapter01/test01.js
+++ chapter01/test01.js
@@ -3,6 +3,28 @@ const fs = require("fs");
 const plays = JSON.parse(fs.readFileSync("./plays.json"));
 const invoices = JSON.parse(fs.readFileSync("./invocies.json"));
 
+function amoutFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return thisAmount;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -13,33 +35,15 @@ function statement(invoice, plays) {
     minumumFractionDigits: 2,
   }).format;
 
-  for (let pref of invoice[0].performances) {
-    const play = plays[pref.playID];
-    let thisAmout = 0;
+  for (let perf of invoice[0].performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amoutFor(perf, play);
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (pref.audience > 30) {
-          thisAmount += 1000 * (pref.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (pref.audience > 20) {
-          thisAmount += 10000 + 500 * (pref.audience - 20);
-        }
-        thisAmount += 300 * pref.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    volumeCredits += Math.max(pref.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(pref.audience / 5);
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
     result += `${play.name}: ${format(thisAmount / 100)} (${
-      pref.audience
+      perf.audience
     }석)\n`;
     totalAmount += thisAmount;
   }
