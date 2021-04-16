7cc1da7b8172abb597fa3e0c4edf9bccd4a8e829
ys9922 <ys9922@naver.com>
2021-04-16 14:43:00 +0900

    statement 함수 안으로 리팩토링 함수들 옮기기

--- chapter01/test01.js
+++ chapter01/test01.js
@@ -3,48 +3,6 @@ const fs = require("fs");
 const plays = JSON.parse(fs.readFileSync("./plays.json"));
 const invoices = JSON.parse(fs.readFileSync("./invocies.json"));
 
-function volumeCreditsFor(aPerformance) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playfor(aPerformance).type)
-    volumeCredits += Math.floor(aPerformance.audience / 5);
-  return volumeCredits;
-}
-
-function playfor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amoutFor(aPerformance) {
-  let result = 0;
-  switch (playfor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playfor(aPerformance).type}`);
-  }
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minumumFractionDigits: 2,
-  }).format(aNumber / 100);
-}
-
 function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -59,6 +17,48 @@ function statement(invoice) {
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playfor(aPerformance).type)
+      volumeCredits += Math.floor(aPerformance.audience / 5);
+    return volumeCredits;
+  }
+
+  function playfor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amoutFor(aPerformance) {
+    let result = 0;
+    switch (playfor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playfor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minumumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 }
 // 객체 형태일때 . 으로 접근 가능
 console.log(invoices[0].performances);
