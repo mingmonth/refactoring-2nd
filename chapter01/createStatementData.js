c3a3d8ab7f330197177be4f8b0859b64dd002684
ys9922 <ys9922@naver.com>
2021-04-21 08:39:48 +0900

    다형성을 활용하여 데이터 생성하기

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -1,44 +1,3 @@
-function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
-}
-
-class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance;
-    this.play = aPlay;
-  }
-
-  amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-    return result;
-  }
-
-  volumeCreditsFor() {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      volumeCredits += Math.floor(this.performance.audience / 5);
-    return volumeCredits;
-  }
-}
-
 export default function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice[0].customer;
@@ -54,8 +13,8 @@ export default function createStatementData(invoice, plays) {
     ); // 생성자 대신 팩터리 함수 이용
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = calculator.amount();
-    result.volumeCredits = calculator.volumeCreditsFor(); // 적립 포인트 계산
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits; // 적립 포인트 계산
     return result;
   }
 
@@ -84,5 +43,56 @@ export default function createStatementData(invoice, plays) {
       aPerformance,
       playFor(aPerformance)
     ).amount();
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error("알 수 없는 장르: ${aPlay.type}");
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
